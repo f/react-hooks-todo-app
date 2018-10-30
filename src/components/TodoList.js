316f387f02b9e9f08ba34281bd97ccb3fcfd062e
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2018-10-30 16:14:14 +0300

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,29 +1,32 @@
 import React, { useContext } from "react";
 import Store from "../context";
-import { TodoHeader } from './TodoHeader';
+import { TodoHeader } from "./TodoHeader";
 
 export default function TodoList() {
   const { state, dispatch } = useContext(Store);
+
+  const pluralize = count =>
+    count > 1 ? `There are ${count} todos.` : `There is ${count} todo.`;
 
   let header =
     state.todos.length === 0 ? (
       <h4>Yay! All todos are done! Take a rest!</h4>
     ) : (
-        <TodoHeader>
-          <span className="float-right">{state.todos.length > 1 ? "There are" : "There is"} {state.todos.length} {state.todos.length > 1 ? "todos" : "todo"}</span>
-        </TodoHeader>
-      );
+      <TodoHeader>
+        <span className="float-right">{pluralize(state.todos.length)}</span>
+      </TodoHeader>
+    );
   return (
     <div className="row">
       <div className="col-md-12">
         <div className="row">
           <div className="col-md-12">
             <br />
-            {header}</div>
+            {header}
+          </div>
         </div>
         <div className="row">
           <div className="col-md-12">
-
             <ul className="list-group">
               {state.todos.map(t => (
                 <li key={t} className="list-group-item">
@@ -34,11 +37,10 @@ export default function TodoList() {
                     onClick={() => dispatch({ type: "COMPLETE", payload: t })}
                   >
                     Complete
-              </button>
+                  </button>
                 </li>
               ))}
             </ul>
-
           </div>
         </div>
       </div>
